b0dba73a89ebbd605e572652c1217af5c1d05d0f
Tom Clarke <tomc@designyourcode.io>
2025-01-09 17:14:24 +0000

    Initial commit for adding Clerk

---
+++ src/app/(frontend)/dashboard/page.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from 'next/types'
+
+export const dynamic = 'force-static'
+export const revalidate = 600
+
+export default async function Page() {
+  return (
+    <div className="pt-24 pb-24">
+      <div className="container mb-16">
+        <div className="prose dark:prose-invert max-w-none">
+          <h1>Dashboard</h1>
+          <p>This will be a protected area by Clerk</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export function generateMetadata(): Metadata {
+  return {
+    title: `Payload Website Template Dashboard`,
+  }
+}
